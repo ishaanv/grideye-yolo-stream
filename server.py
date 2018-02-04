2f2d9dfe1b86605ba8bfef26778d2a3863fb82f8
Ishaan Varshney <ishaan.varshney@gmail.com>
2018-02-05 10:17:18 +1100

    :wrench: properly integrate grideye

--- server.py
+++ server.py
@@ -1,43 +1,66 @@
-import csv
 import json
+import math
+import os
 import time
+from datetime import datetime
+from sys import exit
 
 import numpy as np
 from flask import Flask, render_template
+from scipy.interpolate import griddata
 
 from flask_sockets import Sockets
+from GridEyeKit import GridEYEKit
 
 app = Flask(__name__, static_url_path='')
 app.jinja_env.auto_reload = True
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 sockets = Sockets(app)
 
+# @sockets.route('/recv_data')
+# def data_socket(ws):
+#     while not ws.closed:
+#         data_frames.append(ws.receive())
 
-data_frames = []
-inital_frame = [
-0, 0, 0, 35, 35, 0, 0, 0,
-0, 0, 35, 0, 0, 35, 0, 0,
-0, 0, 0, 0, 0, 35, 0, 0,
-0, 0, 0, 0, 35, 0, 0, 0,
-0, 0, 0, 35, 0, 0, 0, 0,
-0, 0, 0, 35, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 35, 0, 0, 0, 0
-];
+N = 8 #number of pixels per row in original
+M = 32 #number of pixels per row wanted (complex)
+points = [(math.floor(ix / N), (ix % N)) for ix in range(0, N*N)]
+grid_x, grid_y = np.mgrid[0:(N-1):M, 0:(N-1):M]
 
-@sockets.route('/recv_data')
-def data_socket(ws):
-    while not ws.closed:
-        data_frames.append(ws.receive())
+print("Connecting to Grideye")
+try:
+    g = GridEYEKit()
+    g_status_connect = g.connect()
+    print(g_status_connect)
+    if not g_status_connect:
+        g.close()
+        print("could not connect to grideye...")
+        os._exit(1)
+    print("Connected\n")
+except:
+    print("could not connect to grideye...")
+    g.close()
+    os._exit(1)
+
 
 @sockets.route('/serve_data')
 def qa_socket(ws):
     ws.send(json.dumps(inital_frame))
     while not ws.closed:
-        with open('training-Thermal.csv') as f:
-            for frame in csv.reader(f):
-                ws.send(json.dumps(list(map(float, np.reshape(frame, (8,8))))))
-                time.sleep(0.5)
+        pixels = g.get_temperatures()
+        bicubic = griddata(
+            points, pixels.flatten(), (grid_x, grid_y), method='cubic')
+        pixels = bicubic.reshape(32, 32).tolist()
+        therm = g.get_thermistor()
+        current_time = str(datetime.now())
+        data_parse = {"time": current_time, "ambient": therm, "frame": pixels}
+        data = json.dumps(data_parse)
+        ws.send(json.dumps(frame))
+        if not np.any(pixels):
+            import pdb
+            pdb.set_trace()
+        time.sleep(0.5)
+
 
 
 @app.route('/')
