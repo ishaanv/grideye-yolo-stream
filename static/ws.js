0b8c7570fc86b195aceb4e98b6be1177970275fe
Ishaan Varshney <ishaan.varshney@gmail.com>
2018-02-05 14:49:35 +1100

    merge

--- static/ws.js
+++ static/ws.js
@@ -1,19 +1,23 @@
 var ws;
 
-
-Plotly.plot('graph', [{
-    type: 'heatmap',
-    z: [
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0]
-    ]
-}])
+Plotly.d3.json('https://raw.githubusercontent.com/plotly/datasets/master/custom_heatmap_colorscale.json', function(figure) {
+    Plotly.plot('graph', [{
+        type: 'heatmap',
+        z: [
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0]
+        ],
+        zauto: false,
+        zmin: 23,
+        zmax: 27
+    }], {height: 700, width: 700})
+})
 
 function openSocket(url) {
     ws = new WebSocket(url);
@@ -52,51 +56,3 @@ function sendText() {
     ws.send(message);
     document.getElementById('message').value = '';
 }
-
-function finalOutput() {
-    ws.send('done');
-}
-
-function decodeHexString(text) {
-    if (text.search(/[^0-9a-f\s]/i) !== -1) {
-        alert('Can\'t decode "' + text + '" as hexadecimal...');
-    } else {
-        text = text.replace(/\s/g, '');
-        if (text.length % 2 === 1) {
-            text = '0' + text;
-        }
-        var data = [];
-        for (var i = 0, len = text.length; i < len; i += 2) {
-            data.push(parseInt(text.substr(i, 2), 16));
-        }
-        return data;
-    }
-}
-
-function encodeHexString(data) {
-    var bytes = [];
-    for (var i = 0, len = data.length; i < len; i++) {
-        var value = data[i];
-        bytes[i] = value.toString(16);
-        if (value < 16) {
-            bytes[i] = '0' + bytes[i];
-        }
-    }
-    return bytes.join(' ');
-}
-
-function log(message) {
-    var li = document.createElement('li');
-    li.innerHTML = message;
-    document.getElementById('messages').appendChild(li);
-}
-
-$(document).ready(function() {
-    openSocket('ws://localhost:8585/serve_data');
-});
-
-$(document).keypress(function(e) {
-    if (e.which == 13) {
-        sendText();
-    }
-});
